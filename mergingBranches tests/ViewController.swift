5ba046f900650d26c9f46a7df0d3550bb425886f
Vitalii Iakushev <vsiakushev@gmail.com>
2022-11-16 20:23:35 +0200

    Initial Commit

---
+++ mergingBranches tests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mergingBranches tests
+//
+//  Created by Vitaliy Sergeevich on 16.11.2022.
+//  Copyright © 2022 Vitaliy Iakushev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
